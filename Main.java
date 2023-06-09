537d3065fb7c60648e24725a7302362dffe1f3cd
Eduardo Maurício Dias <edudias1972@gmail.com>
2023-06-09 15:24:41 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,31 +1,53 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Laços de repetição - Aula 07 Java - modulo 1");
+        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        // para ir de 1 até 10
-        System.out.println("============Conte de  1 até 10 ===========================");
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("============De dois em dois ===========================");
-        for (int i = 2; i <= 10; i += 2) {
-            System.out.println(i);
-        }
-        System.out.println("============De 10 em 10 ===========================");
-        for (int i = 10; i <= 100; i += 10) {
-            System.out.println(i);
-        }
+        // Criar variavel para receber um nome - > String
+        String nome =  "Eduardo";
+         // letras minusculas
+        System.out.println(nome.toLowerCase());
+        // letras maiusculas
+        System.out.println(nome.toUpperCase());
+        // Contando as letras caracteres
+        System.out.println(nome.length());
 
-        System.out.println("=========== Toda a Tabuada: do 1 ao 10 ===========================");
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(j + "x" + i + "=" + j * i);
-            }
-        }
-    }
+        String nomeOutro = "eduardo";
+        System.out.println(nome.equalsIgnoreCase(nomeOutro));
+
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        Locale brasil = new Locale("pt", "BR");
+        // Locale brasil = new Locale("pt", "BR");
+        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        // System.out.println(diaSemana);
+
+        String saudacao;
+
+        LocalDateTime agora = LocalDateTime.now();
+       //  System.out.println(agora);
+
+       if(agora.getHour() >= 0 && agora.getHour() < 12){
+           saudacao = "bom dia!";
+       } else if (agora.getHour() >= 12 && agora.getHour() < 18){
+            saudacao = "boa tarde!";
+        } else if (agora.getHour() >= 18 && agora.getHour() < 24){
+            saudacao = "boa noite!";
+         } else {
+            saudacao = "Olá.";
+         }
+         //define o formato e valores
+         System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+     }
 }
