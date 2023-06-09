c043bea7689d0a8e6c04ff27803246ca88c45907
Eduardo Maurício Dias <edudias1972@gmail.com>
2023-06-09 15:23:31 -0300

    Laços Numericos

--- Main.java
+++ Main.java
@@ -2,66 +2,30 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Laços de repetição - Aula 07 Java - modulo 1");
 
-        System.out.println("05 - Estruturas Condicionais");
-
-        // 05 - Estruturas Condicionais
-
-        int nota = 100;
-
-        // nota do aluno maior ou igual 70 aluno aprovado
-        // if else
-        if(nota >= 70) {
-            System.out.println("Aluno Aprovado");
-        } else {
-            System.out.println("Aluno Reprovado");
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        // para ir de 1 até 10
+        System.out.println("============Conte de  1 até 10 ===========================");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("============De dois em dois ===========================");
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println("============De 10 em 10 ===========================");
+        for (int i = 10; i <= 100; i += 10) {
+            System.out.println(i);
         }
 
-        // GRADUAÇÃO A 80  B 70 e C 60 e D
-
-        int n = 90 ;
-
-        if(n >= 80) {
-            System.out.println("Graduação A");
-        } else if(n < 80 && n >= 70){
-            System.out.println("Graduação B");
-        } else if(n < 70 && nota >= 60){
-            System.out.println("Graduação C");
-        } else if(n < 60 && nota >= 0 ){
-            System.out.println("Graduação D");
-        } else {
-            System.out.println("Grasuação Invalida");
-        }
-        // GRADUAÇÃO A 80  B 70 e C 60 e D
-
-        int notas = 100;
-        String Graduação = " " ;
-
-        if(notas >= 80) {
-            Graduação=  "A" ;
-        } else if(notas < 80 && notas >= 70){
-             Graduação = "B";
-        } else if(notas < 70 && notas >= 60){
-             Graduação = "C";
-        } else if(notas < 60 && notas >= 0 ){
-            Graduação = "D";
-        } else {
-            Graduação = null ;
-        }
-        // Switch observar o case break e o default
-
-        switch (Graduação){
-            case "A":
-            case "B":
-                System.out.println("Aluno Aprovado");
-                break;
-            case "C":
-            case "D":
-                System.out.println("Aluno Reprovado");
-                break;
-            default:
-                System.out.println("Graduação Invalida");
-                break;
+        System.out.println("=========== Toda a Tabuada: do 1 ao 10 ===========================");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(j + "x" + i + "=" + j * i);
+            }
         }
     }
 }
